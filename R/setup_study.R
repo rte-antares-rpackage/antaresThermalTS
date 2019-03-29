0cce39d2010feb13668adf3e53a996f54ff91827
pvictor <perrier.victor@gmail.com>
2019-03-29 16:00:18 +0100

    updated create study

--- R/setup_study.R
+++ R/setup_study.R
@@ -1,27 +1,44 @@
 
 #' Create an (empty) Antares study for Extended Shutdown Simulation
 #'
-#' @param path Path to a directory where to create study.
+#' @param path Path to an Antares study or to a directory for creating one.
+#' @param start_date Starting date for the study.
 #' @param study_name Name of the study.
 #' @param area_name Name of the area to create
 #'
 #' @export
 #'
 #' @importFrom antaresEditObject createStudy updateGeneralSettings createArea
-#' @importFrom antaresRead setSimulationPath
-setup_study <- function(path, study_name = "prolongation-arrets", area_name = "area") {
-  createStudy(path = path, study_name = study_name)
+#' @importFrom antaresRead setSimulationPath getAreas
+#' @importFrom lubridate month wday year as_date
+setup_study <- function(path, start_date = "2018-07-01", study_name = "prolongation-arrets", area_name = "area") {
   
-  opts <- setSimulationPath(path = path, simulation = "input")
+  if (!dir.exists(path)) {
+    createStudy(path = path, study_name = study_name)
+  }
+  
+  suppressWarnings({
+    opts <- setSimulationPath(path = path, simulation = "input")
+  })
+  
+  start_date <- as_date(start_date)
+  
+  first.month.in.year <- tolower(month(start_date, label = TRUE, abbr = FALSE, locale = "English"))
+  january.1st <- wday(as_date(paste0(year(start_date) + 1, "-01-01")), label = TRUE, abbr = FALSE, locale = "English")
   
   opts <- updateGeneralSettings(
-    first.month.in.year = "july",
+    first.month.in.year = as.character(first.month.in.year),
     first.weekday = "Sunday",
-    horizon = "2018-2019", 
+    january.1st = as.character(january.1st),
+    horizon = paste(year(start_date), year(start_date) + 1, sep = "-"), 
     opts = opts
   )
   
-  opts <- createArea(name = area_name, opts = opts)
+  
+  if (!area_name %in% getAreas()) {
+    opts <- createArea(name = area_name, opts = opts)
+  }
+
   invisible(opts)
 }
 
