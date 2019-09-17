8d3b44962028063e581c2a7d9fbcacb268f2bbc5
pvictor <perrier.victor@gmail.com>
2019-09-17 13:11:06 +0200

    mean thermal ts v2

--- R/mean_thermal_ts.R
+++ R/mean_thermal_ts.R
@@ -55,3 +55,79 @@ mean_thermal_ts <- function(first_weekday = 1, opts = simOptions()) {
 }
 
 
+#' Mean of thermal timeseries by weeks
+#'
+#' @param first_weekday The first day to use for starting a week, default to \code{1} (monday).
+#' @param opts
+#'   List of simulation parameters returned by the function
+#'   \code{setSimulationPath} 
+#'
+#' @return a \code{data.table}.
+#' @export
+#' 
+#' @importFrom antaresRead readClusterDesc
+#' @importFrom lubridate wday hour isoweek
+#' @importFrom data.table first := rbindlist
+#' @importFrom anytime anydate
+#' @importFrom progress progress_bar
+#'
+mean_thermal_ts2 <- function(first_weekday = 1, opts = simOptions()) {
+  clusters <- readClusterDesc(opts = opts)
+  clusters[, area := as.character(area)]
+  clusters[, cluster := as.character(cluster)]
+  clusters[, group := as.character(group)]
+  clusters <- clusters[area == "fr"]
+  pb <- progress_bar$new(
+    format = "  Reading clusters time-series [:bar] :percent",
+    total = nrow(clusters), clear = FALSE
+  )
+  ts_clus <- mapply(
+    FUN = function(area, cluster) {
+      pb$tick()
+      path <- file.path(opts$inputPath, "thermal/series", area, cluster, "series.txt")
+      if (file.size(path) == 0) {
+        return(NULL)
+      }
+      ts <- fread(file = path)
+      ts[, time := seq.POSIXt(from = opts$start, by = "hour", length.out = 8760)]
+      ts[, date := anytime::anydate(time)]
+      ts[, WED19 := FALSE]
+      ts[lubridate::wday(time, week_start = 1) == 3 & lubridate::hour(time) == 19, WED19 := TRUE]
+      ts <- melt(
+        data = ts, 
+        id.vars = c("time", "date", "WED19"), 
+        variable.factor = FALSE, 
+        variable.name = "tsId", 
+        value.name = "ThermalAvailabilities"
+      )
+      ts <- ts[, list(
+        ThermalAvailabilities = mean(ThermalAvailabilities[WED19 == TRUE])
+      ), by = date]
+      ts[, week := lubridate::wday(date, week_start = first_weekday)]
+      ts[week > 1, week := 0]
+      ts[, week := cumsum(week)]
+      ts[lubridate::wday(date, week_start = 1) == 1, nweek := lubridate::isoweek(date)]
+      ts[, nweek := nweek[!is.na(nweek)][1], by = week]
+      tsweek <- ts[, list(
+        ThermalAvailabilities = mean(ThermalAvailabilities, na.rm = TRUE),
+        date = first(date),
+        nweek = first(nweek),
+        n = .N
+      ), by = list(week)]
+      tsweek <- tsweek[n == 7]
+      tsweek[, n := NULL]
+      tsweek[, area := area]
+      tsweek[, cluster := cluster]
+    },
+    area = clusters$area,
+    cluster = clusters$cluster, 
+    SIMPLIFY = FALSE
+  )
+  ts_clus <- rbindlist(ts_clus)
+  ts_clus <- merge(
+    x = ts_clus,
+    y = clusters[, list(area, cluster, group)],
+    by = c("area", "cluster")
+  )
+  ts_clus[]
+}
